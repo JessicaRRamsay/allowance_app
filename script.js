e78ade6617b36ed0b410d308e3129016b3165a73
Jessica Ramsay <jessica@ramsay.org.nz>
2026-08-16 17:59:51 +1200

    Made children private and added some comments

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
 class Family {
+    // Private variables
     #startingBudget
+    #children
 
     constructor(startingBudget) {
         this.#startingBudget = startingBudget
-        this.children = [
+        this.#children = [
             new Child("Aubrie Cooper"),
             new Child("Rangi Cooper"),
             new Child("Manaia Cooper")
         ]
     }
 
-    addNewYearBudget() {
-        for (const child of this.children) {
+    children() {
+        return this.#children
+    }
+
+    // Deposits the children's annual allowance
+    depositAnnualAllowance() {
+        for (const child of this.#children) {
             child.deposit(this.#startingBudget)
         }
     }
 }
 
 class Child {
+    // I have made the balance private to avoid unwanted additions 
     #balance = 0
 
+    // Creates a new child object
     constructor(name) {
         this.name = name;
     }
@@ -29,42 +38,53 @@ class Child {
     }
 
     deposit(amount) {
-        this.#balance = this.#balance + amount
+        this.#balance += amount
     }
 }
 
 class App {
-    #appName = "Allowance app"
+    // Private variables
     #family
 
-    constructor(document) {
-        this.#family = new Family(500)
+    // Creates a new app object
+    constructor() {
+        this.#family = new Family(500) // $500 is the annual allowance
     }
 
+    // This is the apps start up function
     start() {
         if (this.isStartOfYear()) {
-            this.#family.addNewYearBudget()
+            this.#family.depositAnnualAllowance()
+            this.saveBalances()
         }
         else {
             this.loadBalances()
         }
 
-        this.render()
+        this.renderChildPanels()
     }
 
     isStartOfYear() {
-        return true
+        return true // TODO: understand how to detect the start of the year
     }
+
 
     loadBalances() {
-
+        // TODO: load each child's balance from local storage
     }
 
-    render() {
+    saveBalances() {
+        // TODO: Save balances to local storage
+    }
+
+    renderChildPanels() {
+        // Get the child panel template
         let childTemplate = document.getElementById("child-template")
+
+        // Get the element that will hold the 3 children
         let childrenElement = document.getElementById("children")
 
-        for (const child of this.#family.children) {
+        for (const child of this.#family.children()) {
 
             // Make a copy of the child template's content
             let clonedChildElememt = childTemplate.content.cloneNode(true)
@@ -72,14 +92,13 @@ class App {
             // Set child's name
             clonedChildElememt.querySelector(".child-name").textContent = child.name
 
-            // Set child's balence
+            // Set child's balance
             clonedChildElememt.querySelector(".balance-value").textContent = child.balance()
 
-            // Add the child html into the children element
+            // Add the cloned child element into the children element
             childrenElement.append(clonedChildElememt)
         }
     }
-
 }
 
 
