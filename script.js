a574489fb31277e75b6f4a241d681d4a7d4848c7
Jessica Ramsay <jessica@ramsay.org.nz>
2026-08-16 16:20:51 +1200

    Load children from family class and display their names on screen

--- script.js
+++ script.js
@@ -2,15 +2,14 @@ const startingBudget = 500
 
 class Family {
     constructor(name) {
-        this.name = name;
         this.children = [
-            new Child("Aubrie"),
-            new Child("Rangi"),
-            new Child("Manaia")
+            new Child("Aubrie Cooper"),
+            new Child("Rangi Cooper"),
+            new Child("Manaia Cooper")
         ]
     }
-    
-    
+
+
 }
 
 class Child {
@@ -20,14 +19,29 @@ class Child {
 }
 
 class App {
+    #appName = "Jessica's app"
+    #family
+
     constructor(document) {
-        this.document = document
-        this.family = new Family("Cooper")
+        this.#family = new Family()
     }
 
     start() {
-        
+        this.render()
     }
+
+    render() {
+        let childTemplate = document.getElementById("child-template")
+        let childrenElement = document.getElementById("children")
+
+        for (const child of this.#family.children) {
+            let clone = childTemplate.content.cloneNode(true)
+            clone.querySelector(".child-name").textContent = child.name
+            childrenElement.append(clone)
+        }
+    }
+
 }
 
 new App(document).start();
+
